6fba8334a23b8945a9451a1567fe78957d4889d2
JamisonOrdway <jamisonordway@gmail.com>
2018-05-15 14:18:10 -0600

    Implements user story 4

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,6 +17,20 @@ class StudentsController < ApplicationController
     redirect_to students_path
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def student_params
--- spec/features/user_can_edit_a_student_spec.rb
+++ spec/features/user_can_edit_a_student_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 describe 'User' do
   describe 'user navigates to edit student page' do
-    it 'should show a form'
+    it 'should show an edit student form' do
+      student = Student.create(name: 'Sabrina')
+
+
+      visit edit_student_path(student)
+      expect(page).to have_content('Edit student')
+    end
+  end
+  describe 'user fills in student information and clicks submit' do
+    it 'should redirect to student show page with updated information' do
+      student = Student.create(name: 'Matt')
+
+      visit edit_student_path(student)
+      fill_in 'student[name]', with: 'Steve'
+      click_on 'Update Student'
+      expect(current_path).to eq("/students/#{student.id}")
+    end
   end
 end
