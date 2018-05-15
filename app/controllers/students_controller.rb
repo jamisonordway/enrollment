c65723737202589f35dae77e666c77affa04b645
JamisonOrdway <jamisonordway@gmail.com>
2018-05-15 14:27:02 -0600

    Implements user story 4

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -31,6 +31,12 @@ class StudentsController < ApplicationController
     end
   end
 
+  def destroy
+    student = Student.find(params[:id])
+    student.destroy
+    redirect_to students_path
+  end
+
   private
 
   def student_params
---
+++ spec/features/user_can_delete_a_student_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'User' do
+  describe 'user navigates to student index page' do
+    describe 'user clicks delete button next to a students name' do
+      it 'should redirect to students index and that student name should not be on page' do
+        student_1 = Student.create(name: 'Cole')
+        student_2 = Student.create(name: 'Zach')
+        student_3 = Student.create(name: 'Eliot')
+
+        visit students_path
+        save_and_open_page
+        click_on "Delete #{student_3.name}"
+        expect(current_path).to eq('/students')
+        expect(page).to_not have_content(student_3.name)
+      end
+    end
+  end
+end
